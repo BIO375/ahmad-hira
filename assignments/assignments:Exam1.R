ce94f4c6b478d2a7b18239fee634b6a86e0f9f02
hira-ahmad <hiraahmad@Hiras-MacBook-Air.local>
2020-02-20 11:15:36 -0500

    Extra credit

---
+++ assignments/assignments:Exam1.R
@@ -0,0 +1,15 @@
+### Install and load packages ####
+# The following commands will install these packages if they are not already installed, 
+# and then load them!
+
+if(!require(Rmisc)){install.packages("Rmisc")}
+if(!require(DescTools)){install.packages("DescTools")}
+if(!require(boot)){install.packages("boot")}
+if(!require(rcompanion)){install.packages("rcompanion")}
+if(!require(summarytools)){install.packages("summarytools")}
+if(!require(tidyverse)){install.packages("tidyverse")}
+
+SagebrushCrickets <- read_csv("datasets/abd/chapter13chap13e5SagebrushCrickets.csv")
+ggplot(chap13e5SagebrushCrickets) +
+  geom_histogram(aes(timeToMating), binwidth = 2)+
+  facet_wrap(~feedingStatus)
